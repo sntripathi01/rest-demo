0d2d231d99738d1ef8505e1743800b97d99b322d
sntripathi01 <sntripathi01@gmail.com>
2016-08-18 23:19:05 +0530

    code updated
    code updated

--- src/main/java/com/restdemo/service/RestServiceApp.java
+++ src/main/java/com/restdemo/service/RestServiceApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceApp {
 
 	public static void main(String[] args) {
+		
 		System.out.println("=================Service started begin============");
 		System.out.println("=================Service started begin============");
 		System.out.println("=================Service started begin============");
